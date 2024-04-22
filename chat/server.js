92da4de1e7d9e0e16519d607a9913e9cff8c8308
karton <133492548+kartonnn@users.noreply.github.com>
2024-04-22 18:49:59 +0300

    Update server.js

--- chat/server.js
+++ chat/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
-
+#порт
 const port = 6969;
 const server = http.createServer(express);
+#инициализация websocket
 const wss = new WebSocket.Server({ server })
+#подключение к серверу
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(data) {
+    #отправление сообщения 
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(data);
@@ -15,8 +18,9 @@ wss.on('connection', function connection(ws) {
     })
   })
 })
-
+#проверка подключения 
 server.listen(port, function() {
+  #сообщение что проверка прошла успешно
   console.log(`Server is listening on ${port}!`)
 })
 
